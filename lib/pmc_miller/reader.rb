4dec72bc116d1d099e8944b656309c63cacf3f7d
John Duarte <john.duarte@puppet.com>
2020-01-06 12:15:35 -0800

    (SLV-730) Implement reading of JSON for reader
    This commit implements the ability for the PmcMiller::Reader class
    to read JSON files.
    
    Spec test is added for this functionality along with supporting fixture
    tree.

--- lib/pmc_miller/reader.rb
+++ lib/pmc_miller/reader.rb
@@ -4,10 +4,49 @@
 module PmcMiller
   # Reader
   class Reader
+    require "json"
+
     attr_accessor :service
     def initialize(path)
       @path = path
-      self.service = nil
+      @service = nil
+    end
+
+    def read(key)
+      return unless %i[puppetserver puppetdb].include? @service
+
+      results = []
+      json_files.each do |f|
+        host = File.dirname(f).split(File::SEPARATOR).last
+        json_string = File.read(f)
+        data = JSON.parse(json_string)
+        results << data.dig(*key_map(key, host))
+      end
+      results
+    end
+
+    def key_map(key, host)
+      key_map = { puppetdb: {
+        queue_depth: ["servers",
+                      host.gsub(".", "-"),
+                      "puppetdb",
+                      "puppetdb-status",
+                      "status",
+                      "queue_depth"]
+      } }
+      key_map[@service][key]
+    end
+
+    def json_files
+      Dir[File.join(@path, @service.to_s, "*", "*.json")]
+    end
+
+    # List of archive files for defined service
+    # TODO: Unpack and process the contents of these archives with the json
+    # reader.  This archive layout is specified in the
+    # [puppet_metrics_collector](https://github.com/puppetlabs/puppetlabs-puppet_metrics_collector#directory-layout)
+    def archives
+      Dir[File.join(@path, @service.to_s, "*.tar.bz2")]
     end
   end
 end
--- spec/pmc_miller_reader_spec.rb
+++ spec/pmc_miller_reader_spec.rb
@@ -2,11 +2,18 @@
 
 require "pmc_miller/reader"
 
-FIXTURES_DIR = "spec/fixtures"
+FIXTURES_DIR = File.join("spec", "fixtures", "puppet-metrics-collector")
 RSpec.describe PmcMiller::Reader do
   it "allows the setting of the service" do
     reader = PmcMiller::Reader.new(FIXTURES_DIR)
     reader.service = :puppetdb
     expect(reader.service).to eq(:puppetdb)
+  end
+  it "reads service data for given key" do
+    expected_results = [0]
+    reader = PmcMiller::Reader.new(FIXTURES_DIR)
+    reader.service = :puppetdb
+    results = reader.read(:queue_depth)
+    expect(results).to eq(expected_results)
   end
 end
