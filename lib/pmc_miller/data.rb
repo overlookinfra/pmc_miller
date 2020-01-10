0d3437782b92caed7479890fcb60992e822aa549
John Duarte <john.duarte@puppet.com>
2020-01-10 12:00:46 -0800

    (SLV-730) Add yard docs for PmcMiller::Data

--- lib/pmc_miller/data.rb
+++ lib/pmc_miller/data.rb
@@ -3,16 +3,31 @@
 require "time"
 
 module PmcMiller
-  # DataPoint object
+  ##
+  # DataPoints are a defined struct containing an iso8601 time and a value.
+  #
+  # @param time_string [String] Timestamp associated with value
+  # @param value [String, Integer, Float] Value of found data
+  #
+  # @return [PmcMiller::DataPoint] The resulting DataPoint
+  #
   DataPoint = Struct.new(:time_string, :value) do
     def time
       Time.iso8601(time_string)
     end
   end
 
+  ##
   # Collection of DataPoint objects
+  #
+  # @return [PmcMiller::Data] Object to store PmcMiller::DataPoint objects
+  #
   class Data < Array
-    # Return the element from the middle of the array.
+    ##
+    # Return the element from the middle of the Data object.
+    #
+    # @return [PmcMiller::DataPoint]
+    #
     def mid
       self[(length / 2)]
     end
